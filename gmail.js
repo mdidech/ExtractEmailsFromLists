c6d07209bd58a1ada19582ae1e0993decd16a82d
mdidech <abdelaziz.mdidech@gmail.com>
2018-04-17 11:17:47 +0000

    extract emails

---
+++ gmail.js
@@ -0,0 +1,30 @@
+let fs=require('fs');
+let list=[];
+let isp='gmail';
+let writeF=fs.writeFileSync;
+let wfile=`./dataAdamGo/${isp}USA.txt`;
+let readMe=fs.readFileSync(`data4.txt`, 'utf8'); 
+// form of lines in files: |||||||name|date|didech.abdel@gmail.com|gfhgf|56787
+let lines = readMe.split('\n');
+let emails=(line,pos)=>{
+    for (let index = pos; index > 0; index--) {
+        if (line.substring(index, index - 1) === '|') {
+            return line.substring(index, pos + 10);
+            break;
+        }
+    }
+}
+lines.forEach((line) => {
+    let pos = line.indexOf(`@${isp}.com`);
+    if(line.indexOf(`@${isp}.com`)>0){
+        list.push(emails(line,pos));
+        console.log(emails(line,pos));
+    }
+   
+})
+    console.log("-------------------------------------------------------------------------");
+    console.log(list.length);
+    writeF(wfile,list.join('\n'));
+    console.log("DONE");
+    console.log("-------------------------------------------------------------------------");
+    
